d2c4d83cc7db18b4d3c00a7259ef97796889671a
Haritz Otero <96473981+Haritzote@users.noreply.github.com>
2022-01-07 12:18:28 +0100

    Add files via upload

---
+++ MAIN.py
@@ -0,0 +1,14 @@
+
+print("THE CALENDAR MENU (Choose a task typing its number!)")
+print("==================================================")
+print("1--Print calendar")
+print("2--Add event")
+print("3--Remove events")
+print("4--Search if a day have events or not")
+print("5--Search event by name")
+print("6--Show events")
+print("7--Playu Rock,Paper,Scissors")
+print("8--Show how maby events are added.")
+print("7--Exit")
+
+
