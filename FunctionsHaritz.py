6d642eb73a8c4a83a661b5c077fb1367aebb9304
Haritz Otero <96473981+Haritzote@users.noreply.github.com>
2022-01-18 12:30:26 +0100

    FunctionsHaritz V3

--- FunctionsHaritz.py
+++ FunctionsHaritz.py
@@ -10,12 +10,13 @@ def printCalendar ():
     print("6--Show events")
     print("7--Playu Rock,Paper,Scissors")
     print("8--Show how many events are added.")
-    print("7--Exit")
+    print("7--Drawing machine")
+    print("9--Exit")
 
-def addEvents ():
+def addEvents (events):
 
     print("You have chosen to add an event.")
-    events = []
+
 
     event_num = int(input("How many events you want to add?"))
 
@@ -24,3 +25,57 @@ def addEvents ():
         event = input("Enter the event you have and specify thr date.")
         events.append(event)
         i += 1
+def drawingMachine ():
+
+    print("WELCOME TO THE DRAWING MACHINE!")
+    print("=========================")
+    print("What do you want to draw?")
+    print("=========================")
+    print("1.- Drawing a pyramid.")
+    print("2.- Drawing a triangle.")
+    print("3.- Dawing a rombous.")
+    print("4.- Drawing a number trinagle.")
+    print("5.- Exit program.")
+    print("Enter the number of the exercise: ")
+
+    def pyramid ():
+        altuera = input("Sartu piramidearen altuera.")
+
+
+    printf("Enter the height of the pyramid: ");
+    scanf("%d", & altuera);
+
+    int
+    i, j;
+
+
+for (i=1;i <= altuera;i++){
+for (j=1;j <= altuera-i;j++){
+printf(" ");
+}
+
+for (j=1;j <= 2 * i-1;j++){
+printf("*");
+}
+printf("\n");
+
+}
+}
+
+
+while option != 5 :
+            if option == 1:
+            pyramid()
+
+            if option == 2:
+            triangle()
+
+            if option == 3 :
+            rombous()
+
+            if option == 4:
+            numberTriangle()
+
+        option = input("Please, enter the number depending of the drawing you want to draw.")
+
+    }
